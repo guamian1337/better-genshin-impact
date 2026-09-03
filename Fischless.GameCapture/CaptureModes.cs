a8d8cbed688888732f73cf618a423c130319f8e3
guamian <92618050+guamian1337@users.noreply.github.com>
2026-09-03 09:42:37 +0800

    Update CaptureModes.cs

--- Fischless.GameCapture/CaptureModes.cs
+++ Fischless.GameCapture/CaptureModes.cs
@@ -20,7 +20,7 @@ public enum CaptureModes
     [DefaultValue(3)]
     WindowsGraphicsCaptureHdr = 3,
 
-    [Description("WindowsGraphicsCapturev2")]
+    [Description("WindowsGraphicsCapture V2")]
     [DefaultValue(5)]
     WindowsGraphicsCaptureV2 = 5,
 }
